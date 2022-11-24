9df409c9ba2879595593ba61b7a0a1807ec21463
Dmitry Yushin <youshin.d@gmail.com>
2022-11-24 22:21:31 +0300

    system input examples update

--- src/main/java/ru/evolenta/course/SysInDemo.java
+++ src/main/java/ru/evolenta/course/SysInDemo.java
@@ -2,18 +2,33 @@ package ru.evolenta.course;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class SysInDemo {
     public static void main(String[] args) throws IOException {
 
-        System.out.println(System.in.read());
-        System.out.println((char) System.in.read());
+//        System.out.println(System.in.read());
+//        System.out.println((char) System.in.read());
 
         Scanner in = new Scanner(System.in);
+
+        System.out.print("строка: ");
+        System.out.println(in.nextLine());
+
+        System.out.print("слово: ");
+        System.out.println(in.next());
+
         int a = in.nextInt();
         int b = in.nextInt();
 
         System.out.println(a + b);
+
+        in.useLocale(Locale.ENGLISH);
+        double dbl = in.nextDouble();
+
+        in.useRadix(16);
+        int n = in.nextInt();
+        System.out.println(n);
     }
 }
