b6f4c124f34aec30bb52d758ae55309518b472c6
yushin <d.ushin@evolenta.ru>
2022-12-01 17:04:49 +0300

    наследование и перегрузка метода

---
+++ src/main/java/oop/ExtCalculator.java
@@ -0,0 +1,8 @@
+package oop;
+
+public class ExtCalculator extends Calculator {
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+}
--- src/main/java/oop/Main.java
+++ src/main/java/oop/Main.java
@@ -4,22 +4,8 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        //Person p = new Person("Иванов", "Иван", "Иванович", "1999-05-07", "МУЖСКОЙ");
-        Person[] people = {
-                new Person("Иванов", "Иван", "Иванович", LocalDate.parse("1999-05-07"), Gender.MALE),
-                new Person("Иванова", "Иванесса", "Ивановна", LocalDate.parse("1999-05-07"), Gender.FEMALE),
-                new Person("Петров", "Пётр", "Петрович", LocalDate.parse("2001-01-02"), Gender.MALE),
-                new Person("Калачина", "Лариса", "Олеговна", LocalDate.parse("1989-11-07"), Gender.FEMALE),
-                new Person("Иванов", "Иван", "Иванович", LocalDate.parse("2017-05-07"), Gender.MALE),
-        };
-
-        for (Person p : people) {
-            if (p.getGender().equals(Gender.MALE) && LocalDate.now().getYear() - p.getBirthday().getYear() >= 18) {
-                System.out.println(p);
-            }
-        }
-
-        Calculator calculator = new Calculator();
-        System.out.println(9 / 7);
+        ExtCalculator calculator = new ExtCalculator();
+        System.out.println(calculator.divide(9.0, 7.0));
+        System.out.println(calculator.divide(9, 7));
     }
 }
