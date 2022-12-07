58b738e43bb8978e5cce5893fbd2702ce9983def
Dmitry Yushin <youshin.d@gmail.com>
2022-12-07 22:16:34 +0300

    more example generics

--- src/main/java/ru/evolenta/course/generic/ExampleExtends.java
+++ src/main/java/ru/evolenta/course/generic/ExampleExtends.java
@@ -8,15 +8,51 @@ import java.time.LocalDate;
 
 public class ExampleExtends {
     public static void main(String[] args) {
+        //type erase
+        //rawType
+        ArrayDynamic arrayDynamic = new ArrayDynamic();
+        //generic
         ArrayDynamic<Person> persons = new ArrayDynamic<>();
+        arrayDynamic = persons;
+        arrayDynamic.add(22);
+        //Person p = persons.get(0); //java.lang.ClassCastException
+        //System.out.println(p);
+
+        //don't extends
+        ArrayDynamic<Object> objectArray = new ArrayDynamic<>();
+        ArrayDynamic<String> stringArray = new ArrayDynamic<>();
+        //objectArray = stringArray; //error
+
+        //extends
+        class Box <T extends Person> {
+            void outName(T person) {
+                System.out.println(person.getName());
+            }
+
+            <E> void outElement(E element) {
+                System.out.println(element.getClass().getName());
+            }
+        }
+
         Student student = new Student("Ivanov", "Ivan", "Ivanovich", LocalDate.of(1999, 9, 1), Gender.MALE);
+        Box<Student> studentBox = new Box<>();
+        studentBox.outName(student);
+        studentBox.outElement(student);
+
+        //Student student = new Student("Ivanov", "Ivan", "Ivanovich", LocalDate.of(1999, 9, 1), Gender.MALE);
         persons.add(student);
         System.out.println(persons);
 
         add(persons, student);
         System.out.println(persons);
+
+        ArrayDynamic<Person> to = new ArrayDynamic<>(); //Student - error
+        ArrayDynamic<Student> from = new ArrayDynamic<>();
+        from.add(student);
+        copy(to, from);
     }
 
+    //Wildcard
     //PECS (producer - extends, consumer - super)
 /*
 
@@ -35,12 +71,19 @@ public class ExampleExtends {
 
 */
 
+//    upper bound          ? extends SuperType
     public static Person getByIndex(ArrayDynamic<? extends Person> arrayDynamic, int index) {
         return arrayDynamic.get(index);
     }
 
+//    lower bound          ? super   SubType
     public static void add(ArrayDynamic<? super Person> arrayDynamic, Person person) {
         arrayDynamic.add(person);
+    }
+
+    public static void copy(ArrayDynamic<? super Person> to, ArrayDynamic<? extends Person> from) {
+        to.add(from.get(0));
+        System.out.println(to);
     }
 
 }
