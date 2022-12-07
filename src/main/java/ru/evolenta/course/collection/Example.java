5e2271729f45a6eea8468894957142bd600ba074
Dmitry Yushin <youshin.d@gmail.com>
2022-12-07 23:05:50 +0300

    init collection examole

---
+++ src/main/java/ru/evolenta/course/collection/Example.java
@@ -0,0 +1,18 @@
+package ru.evolenta.course.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+        //Iterable
+        //Collection
+        //List //ArrayList, LinkedList, EnumsList //set, get, indexOf
+
+        //Set //TreeSet //HashSet
+
+        //Map //HashMap //TreeMap //get, put //Set<K> keySet //Collection<V> values
+    }
+}
