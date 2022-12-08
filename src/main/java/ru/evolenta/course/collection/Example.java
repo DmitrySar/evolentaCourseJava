db4333c970e77af4565d8753b67f5cbc7302caca
yushin <d.ushin@evolenta.ru>
2022-12-08 14:56:56 +0300

    add examples to collections theme

--- src/main/java/ru/evolenta/course/collection/Example.java
+++ src/main/java/ru/evolenta/course/collection/Example.java
@@ -1,18 +1,71 @@
 package ru.evolenta.course.collection;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import ru.evolenta.course.oop.Gender;
+import ru.evolenta.course.oop.Person;
+import ru.evolenta.course.oop.Student;
+
+import java.time.LocalDate;
+import java.util.*;
 
 public class Example {
     public static void main(String[] args) {
         //Iterable
-        //Collection
-        //List //ArrayList, LinkedList, EnumsList //set, get, indexOf
+        Iterable iterable = List.of(34, 56, 2, 78, -1);
 
-        //Set //TreeSet //HashSet
+        //Iterator
+        Iterator<Integer> iterator = iterable.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        //Collection
+        Collection collection = List.of(45, -89, 56, 34);
+        //collection.add(33); //UnsupportedOperationException
+        System.out.println(collection.contains(3));
+
+        //List //ArrayList, LinkedList //set, get, indexOf
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(-1);
+        list.add(89);
+        list.add(55);
+        System.out.println(list.remove(2)); //?
+        System.out.println(list);
+
+        ListIterator<Integer> listIterator = list.listIterator(0);
+//        System.out.println(listIterator.previous()); //NoSuchElementException
+        System.out.println(listIterator.next());
+        System.out.println(listIterator.previous());
+
+        //Set //TreeSet //HashSet //EnumSet
+        Set set = Set.of("i", "a", "r", "w", "-1");
+        System.out.println(set); //не гарантирует порядок элементов
+
+        //TreeSet - отсортирован
+        Set<Integer> numbersSet = new TreeSet<>();
+        numbersSet.add(45);
+        numbersSet.add(-23);
+        numbersSet.add(99);
+        System.out.println(numbersSet);
+
+        EnumSet<WeekDays> workDays = EnumSet.of(WeekDays.MON, WeekDays.TUE, WeekDays.WED, WeekDays.THU, WeekDays.FRI);
+        System.out.println(workDays.contains(WeekDays.SUN));
 
         //Map //HashMap //TreeMap //get, put //Set<K> keySet //Collection<V> values
+        Map<String, ? super Person> personsMap = new HashMap<>();
+        personsMap.put("one", new Student("Petrov", "Petr", "Petrovich", LocalDate.of(1998, 03, 07), Gender.MALE));
+        personsMap.put("two", new Student("Sidorova", "Valeriya", "Olegovna", LocalDate.of(1978, 05, 01), Gender.FEMALE));
+        personsMap.put("three", new Student("Abrikosov", "Michael", "Victorovich", LocalDate.of(2001, 01, 02), Gender.MALE));
+
+        System.out.println(personsMap);
+
+        for (Map.Entry studentEntry : personsMap.entrySet()) {
+            System.out.printf("%-10s: ", studentEntry.getKey());
+            System.out.println(studentEntry.getValue());
+        }
     }
+}
+
+enum WeekDays {
+    MON, TUE, WED, THU, FRI, SAT, SUN
 }
